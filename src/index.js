6a2066e72ec789fba5b4afea302aec8fec0998d2
Michał Janeczek <michal.janeczek@ymail.com>
2016-07-09 17:30:31 +0200

    Dynamic interactors implementation

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ export { conventionalReduxMiddleware } from './middleware';
 export { conventionalReducers } from './reducer';
 
 export var interactors = {};
+export var dynamicInteractorKeys = [];
+export var recreateReducerFunction = null;
 
-export function registerInteractors(interactors) {
+export function registerInteractors(interactors, options = {}) {
   for (const name in interactors){
-    registerInteractor(name, interactors[name]);
+    registerInteractor(name, interactors[name], options);
   }
 }
 
-export function registerInteractor(key, interactor) {
+export function registerInteractor(key, interactor, options = {}) {
+  if(options['dynamic']) {
+    dynamicInteractorKeys.push(key)
+  }
+
   interactors[key] = interactor;
+}
+
+export function replaceDynamicInteractors(interactors) {
+  removeDynamicInteractors();
+  registerInteractors(interactors, { dynamic: true });
+
+  // add error if function is missing
+  recreateReducerFunction();
+}
+
+export function removeDynamicInteractors() {
+  dynamicInteractorKeys.forEach(key => {
+    delete interactors[key]
+  })
+}
+
+export function setRecreateReducerFunction(func) {
+  recreateReducerFunction = func;
 }
