b9285d59400c1e3288a6c7f6ebb049c6f3b5cff6
Michal Janeczek <michal.janeczek@ymail.com>
2018-01-16 13:22:27 +0100

    Store implementation

--- src/store.js
+++ src/store.js
@@ -1,5 +1,42 @@
-export default class {
+export default class Store {
+  registeredInteractors = {};
+  dynamicInteractorKeys = [];
+  recreateReducerFunction = null;
+
   interactors() {
-    return [];
+    return this.registeredInteractors;
+  }
+
+  registerInteractor(name, interactor, options = {}) {
+    if(options['dynamic']) {
+      this.dynamicInteractorKeys.push(name)
+    }
+
+    this.registeredInteractors[name] = interactor;
+  }
+
+  registerInteractors(interactors, options = {}) {
+    for (const name in interactors) {
+      this.registerInteractor(name, interactors[name], options);
+    }
+  }
+
+  removeDynamicInteractors() {
+    this.dynamicInteractorKeys.forEach(key => {
+      delete this.registeredInteractors[key]
+    })
+  }
+
+  setRecreateReducerFunction(func) {
+    this.recreateReducerFunction = func;
+  }
+
+  replaceDynamicInteractors(interactors) {
+    this.removeDynamicInteractors();
+    this.registerInteractors(interactors, { dynamic: true });
+
+    if(this.recreateReducerFunction) {
+      this.recreateReducerFunction();
+    }
   }
 }
--- tests/store.test.js
+++ tests/store.test.js
@@ -1,6 +1,82 @@
 import Store from '~/store';
+import FakeInteractor from './stubs/fake_interactor';
 
-test('no interactors by default', () => {
-  const store = new Store();
-  expect(store.interactors()).toEqual([]);
+describe('interactor store', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Store();
+  });
+
+  test('no interactors by default', () => {
+    expect(store.interactors()).toEqual({});
+  });
+
+  test('registers an interactor', () => {
+    const projectsInteractor = new FakeInteractor();
+
+    store.registerInteractor('projects', projectsInteractor);
+    expect(store.interactors()['projects']).toEqual(projectsInteractor);
+  });
+
+  test('registers many interactors', () => {
+    const projectsInteractor = new FakeInteractor();
+    const usersInteractor = new FakeInteractor();
+
+    store.registerInteractors({projects: projectsInteractor, users: usersInteractor});
+
+    expect(store.interactors()['projects']).toEqual(projectsInteractor);
+    expect(store.interactors()['users']).toEqual(usersInteractor);
+  });
+
+  describe('dynamic option', () => {
+    test('registers a dynamic interactor', () => {
+      const projectsInteractor = new FakeInteractor();
+
+      store.registerInteractor('projects', projectsInteractor, { dynamic: true });
+      expect(store.interactors()['projects']).toEqual(projectsInteractor);
+    });
+
+    test('registers many dynamic interactor', () => {
+      const projectsInteractor = new FakeInteractor();
+      const usersInteractor = new FakeInteractor();
+
+      store.registerInteractors({projects: projectsInteractor, users: usersInteractor}, { dynamic: true });
+
+      expect(store.interactors()['projects']).toEqual(projectsInteractor);
+      expect(store.interactors()['users']).toEqual(usersInteractor);
+    });
+
+    test('removes all dynamic interactors', () => {
+      const projectsInteractor = new FakeInteractor();
+      const usersInteractor = new FakeInteractor();
+
+      store.registerInteractor('projects', projectsInteractor, { dynamic: true });
+      store.registerInteractors({users: usersInteractor}, { dynamic: true });
+
+      store.removeDynamicInteractors();
+      expect(store.interactors()).toEqual({});
+    });
+
+    test('replaces dynamic interactors', () => {
+      const projectsInteractor = new FakeInteractor();
+      const usersInteractor = new FakeInteractor();
+
+      store.registerInteractors({projects: projectsInteractor, users: usersInteractor}, { dynamic: true });
+
+      const clientsInteractor = new FakeInteractor();
+      store.replaceDynamicInteractors({clients: clientsInteractor});
+
+      expect(Object.keys(store.interactors())).toEqual(['clients']);
+    });
+
+    test('sets recreate reducer function', () => {
+      const mockCallback = jest.fn();
+
+      store.setRecreateReducerFunction(mockCallback);
+      store.replaceDynamicInteractors({});
+
+      expect(mockCallback.mock.calls.length).toBe(1);
+    });
+  });
 });
